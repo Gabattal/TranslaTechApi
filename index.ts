4f55397adb8b60cd5249d7c8bd012c7d8e349e65
Gabriel Attal <gab-attal@hotmail.fr>
2023-05-01 18:24:57 +0200

    fix

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import fastify, { FastifyRequest, FastifyReply } from 'fastify';
+import fastify, {FastifyRequest, FastifyReply} from 'fastify';
 import createTranslateHandler from 'services/translate/handlers/translateDocument'
 import cors from '@fastify/cors';
 import fastifyMultipart from '@fastify/multipart';
@@ -8,39 +8,40 @@ const app = fastify().withTypeProvider<TypeBoxTypeProvider>();
 
 export type TServer = typeof app;
 
-async function initServer(server:TServer){
+async function initServer(server: TServer) {
     const plugins = [
-        { plugin: cors, options: { origin: ['*'] } },
+        {plugin: cors, options: {origin: ['*']}},
     ];
-    plugins.forEach(({ plugin, options }) => server.register(plugin, options));
+    plugins.forEach(({plugin, options}) => server.register(plugin, options));
 
     server.register(fastifyMultipart, {
         addToBody: true,
     });
 
     server.post('/upload', async (request: FastifyRequest, reply: FastifyReply) => {
-        const { fileInput } = request.body;
+        const {fileInput} = request.body;
         const content = JSON.parse(fileInput[0].data.toString());
         return content;
     });
 
     server.get('/test', async (request: FastifyRequest, reply: FastifyReply) => {
-        return 'coucou';
+        return {title: 'coucou'};
     });
     await createTranslateHandler(server)
     return server
 }
 
-initServer(app).then((server)=>{
-    server.listen({
-        port: 3000,
-        host: 'localhost',
-    }, (err, address) => {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        console.log(`Server listening on ${address}`);
-        console.log(server.printRoutes());
-    });
-})
+initServer(app)
+    .then(server => {
+        const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+        const host = 'localhost';
+
+        server.listen(port, host, (err, address) => {
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            }
+            console.log(`Server listening on ${address}`);
+            console.log(server.printRoutes());
+        });
+    })
